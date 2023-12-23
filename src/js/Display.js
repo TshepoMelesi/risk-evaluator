296f6f3396a0dc8ae5fcd988272516b4bd3ba570
TshepoMelesi <twmelesi@gmail.com>
2023-12-23 14:40:56 +0200

    added trade logs and stats

--- src/js/Display.js
+++ src/js/Display.js
@@ -1,10 +1,27 @@
 class Display{
-        constructor(app){
+        constructor( app ){
                 this.app = app;
 
-                console.log( "display class" );
+        }
+        logTrades( _trade ){
+                let _growth = Math.round( 
+                                                        ( ( _trade.newBalance - this.app.deposit ) 
+                                                        / this.app.deposit ) * 100 );
+                let tradeTemp = `
+                <div class="trade">
+                        <p class="balance">${ _trade.initBalance }</p>
+                        <p class="gain ${ _trade.tradeResult < 0 ? "loss" : "win" }">
+                        ${ _trade.tradeResult }</p>
+                        <p class="new-balance">
+                        ${ _trade.newBalance <= 0 ? 0 : _trade.newBalance }</p>
+                        <p class="growth">
+                        ${ _growth }
+                        </p>
+                </div>
+                `;
+                this.app.tradesList.innerHTML += tradeTemp;
         }
 }
 
-export default Display
+export default Display;
 
